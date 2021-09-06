9bce5091fb7cf23705eb967b31233cc531876149
Shadrack Ongera <shadrack.sme@gmail.com>
2021-09-06 13:16:54 +0300

    episode 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -20,6 +20,7 @@
                     <div class="card">
 
                         <div class="panel-heading">
+                            <a href="#">{{$reply->owner->name}}</a> <br>
                             {{$reply->created_at->diffForHumans()}}
                         </div>
                         <hr>
